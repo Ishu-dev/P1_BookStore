17586df48d5c51a49a0cda4738ea8bcc2fb7472d
Ishu Chaudhary <dev.ishuchaudhary.ind@gmail.com>
2023-09-12 11:49:42 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://root:root@books-store-mern.bvgg5va.mongodb.net/books-collection?retryWrites=true&w=majority";
+
+// export const mongoDBURL =
+//   "mongodb+srv://root:<password>@books-store-mern.bvgg5va.mongodb.net/?retryWrites=true&w=majority";
